412aadc8afa2c3d5041bd5030f2606b13eade846
mredzmi <mredzmi@gmail.com>
2023-09-10 21:05:38 +0800

    test

---
+++ src/test/steps/loginSteps.ts
@@ -0,0 +1,42 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import { chromium, Page, Browser, expect } from "@playwright/test";
+
+
+let browser: Browser;
+let page: Page;
+
+Given('User navigates to the application', async function () {
+     browser = await chromium.launch({ headless:false })
+     page = await browser.newPage();
+     await page.goto("https://bookcart.azurewebsites.net/");
+  });
+
+Given('User click on the login link', async function () {
+    await page.locator("//span[text()='Login']").click();
+  });
+
+Given('User enter the username as {string}', async function (username) {
+    await page.locator("input[formcontrolname='username']").type(username);
+
+    });
+
+Given('User enter the password as {string}', async function (password) {
+    await page.locator("input[formcontrolname='password']").type(password);
+
+    });
+
+When('User click on the login button', async function () {
+    await page.locator("button[color='primary']").click();
+    });
+
+Then('Login should be success', async function () {
+const text = await page.locator("//button[contains(@class,'mat-focus-indicator mat-menu-trigger')]//span[1]").textContent();
+console.log("Username: " + text) ;
+    await browser.close();
+    });
+
+When('Login should fail', async function () {
+  const failureMessages = page.locator("mat-error[role='alert']");
+  await expect(failureMessages).toBeVisible();
+  await browser.close();
+    });  
--- tests/my_first_test.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect} = require('@playwright/test');
-
-test('My first test ', async ({page}) => {
-
-        await page.goto('https://coingecko.com');
-        await expect(page).toHaveTitle('Cryptocurrency Prices, Charts, and Crypto Market Cap | CoinGecko');
-})
